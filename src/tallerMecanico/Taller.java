e2eee23f44baeefc19ccd77c1f886f0c902efab9
sselvaggi <sergio.selvaggi@gmail.com>
2016-08-19 19:34:54 -0300

    Initail commit, menu options

---
+++ src/tallerMecanico/Taller.java
@@ -0,0 +1,19 @@
+package tallerMecanico;
+
+public class Taller {
+
+	public static void main(String[] args) {
+
+		System.out.println("ingrese una opción:");
+		System.out.println("1 Registrar un trabajo");
+		System.out.println("2 Mostrar trabajos pendientes");
+		System.out.println("3 Dar de baja un trabajo");
+		System.out.println("4 Mostrar trabajos finalizados");
+		System.out.println("5 Calcular precio de trabajo");
+		System.out.println("6 Registrar horas");
+		System.out.println("7 Registrar materiales");
+		System.out.println("8 Verificar horas disponibles");
+		System.out.println("9 Totalizar descuentos");
+	}
+
+}
